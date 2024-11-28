72e81ec0c18635a58a6140d2e33853858944672e
eli <john.berengu@student.moringaschool.com>
2024-11-28 20:18:20 +0300

    lab completed

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  let results
+   if (distance > 2500){
+    results = 'No can do.'
+  }
+  else if (distance >= 2000){
+    results = 'I will gladly take your thirty bucks.'
+  }
+  else if (distance <= 2000 && distance>= 400){
+    results = 'That will be twenty bucks.'
+  }
+  if (distance <= 400){
+    results = 'This one is on me!'
+  }
+  return results
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let results
+  if (city === 'NYC')  {
+    results = 'Ok, sounds good.';
+  }
+  else {
+    results = "No go.";
+}
+return results
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let results
+  if ( tip === 'generous')  {
+    results = "Thank you so much.";
+  }
+  else if (tip === 'not as generous'){
+    results = "Thank you."
+  }
+  else {
+    results = "Bye.";
+}
+return results
 }
